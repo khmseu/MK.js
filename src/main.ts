d7f4fe91f5d41ad744e67c0b5f9a1a344e0328a2
Kai Henningsen <kai.extern+git@gmail.com>
2021-01-08 21:40:38 +0100

    Some more experimental code

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { readFile, realpath } from "fs/promises";
+import { join } from "path";
+import { cwd } from "process";
+import { RuleObject } from "./RuleObject";
+
+let top;
+
+async function readRules(dirName: string) {
+  const fullfn = join(dirName, "mk.json");
+  const src = await readFile(fullfn, { encoding: "utf-8" });
+  const obj: RuleObject = JSON.parse(src);
+  return obj;
+}
+
+async function main() {
+  const top = await realpath(cwd());
+  const trobj = await readRules(top);
+}
+
+main()
+  .then(() => console.log("Ok"))
+  .catch((e) => console.error(e));
